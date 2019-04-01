34ed024cb2862b6ce6090a829b15e4b12d196b7c
practicas-ging <practicas-ging@mail.com>
2019-04-01 09:31:54 +0200

    Updated 04_quiz_bbdd

--- tests/compress.js
+++ tests/compress.js
@@ -1,8 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
 const archiver = require('archiver');
+
 const orig = path.resolve(path.join(__dirname, "CORE19-04_quiz_bbdd.zip"));
-console.log(__dirname);
 const dest = path.resolve(path.join(__dirname ,"../CORE19-04_quiz_bbdd.zip"));
 const output = fs.createWriteStream(orig);
 const archive = archiver('zip', {
